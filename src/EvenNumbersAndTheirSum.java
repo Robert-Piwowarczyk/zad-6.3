1be9cb5af10846237d989eb68960a78acb14f5cd
HelloWorld <robpiw@op.pl>
2020-04-23 18:30:22 +0200

    Liczby parzyste i ich suma

---
+++ src/EvenNumbersAndTheirSum.java
@@ -0,0 +1,14 @@
+public class EvenNumbersAndTheirSum {
+    public static void main(String[] args) {
+
+        for (var i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+}
+
+
